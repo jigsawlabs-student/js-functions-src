5dce409f575717c18f70b817627e0e9730b81a2f
JeffKatzy <jeffkatzygit config --global user.name JeffKatzy>
2022-01-09 11:16:36 -0500

    add all

---
+++ index.js
@@ -0,0 +1,9 @@
+function greeting(){
+    return 'hello world'
+}
+
+// let presidents = ['washington', 'adams', 'jefferson']
+
+// presidents.forEach(function (president) { 
+//     console.log(`Here is ${president}`)
+// })
